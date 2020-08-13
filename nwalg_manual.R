b7a31f4b8d37c0920ea09a66da461c4e4aa187e2
Bentoske <ajmill38@asu.edu>
2020-08-13 15:54:47 -0700

    Initial commit

---
+++ nwalg_manual.R
@@ -0,0 +1,119 @@
+
+
+
+# string to vector
+seq1.st <- "ACCGGTAT"
+seq2.st <- "ACCTATC"
+
+seq1 <- c(NA, strsplit(seq1.st, split = "", useBytes = TRUE)[[1]])
+seq1
+seq2 <- c(NA, strsplit(seq2.st, split = "", useBytes = TRUE)[[1]])
+seq2
+
+# ---------------------------- build data frame
+aln1 <- matrix(, nrow = length(seq1), ncol = length(seq2))
+rownames(aln1) <- seq1
+colnames(aln1) <- seq2
+aln1.df <- as.data.frame(aln1)
+aln1.df
+
+# ---------------------------- copy data frame
+traceback.df <- as.data.frame(aln1.df)
+traceback.df
+
+# ---------------------------- assign variables
+gap <- -1
+match <- 2
+mismatch <- -2
+b <- 0
+r <- 0
+d <- 0 
+
+
+# ---------------------------- int rows
+for(i in 1:length(seq2)){
+  aln1.df[1,i] <- gap * (i-1)
+  traceback.df[1,i] <- "R"
+}
+aln1.df
+traceback.df
+
+# ---------------------------- int columns
+for(i in 1:length(seq1)){
+  aln1.df[i,1] <- gap * (i-1)
+  traceback.df[i,1] <- "B"
+}
+aln1.df
+traceback.df
+
+# ---------------------------- rest of matrix
+for(i in 2:length(seq1)){
+  for(j in 2:length(seq2)){
+    b <- aln1.df[i-1, j] + gap 
+    r <- aln1.df[i, j-1] + gap 
+    if(seq1[i] == seq2[j]){
+      d <- aln1.df[i-1, j-1] + match
+    } else {
+      d <- aln1.df[i-1, j-1] + mismatch
+    }
+  aln1.df[i,j] <- max(b,r,d)
+ # traceback.df[i,j] <- ifelse(which.max(b,r,d) == b,  )
+  }
+}
+aln1.df
+
+# ---------------------------- traceback
+
+?which.max # explore this 
+
+# Create df for values to generate into 
+tb.aln <- matrix (nrow = 2, ncol = length(seq1) - 1)
+final.df <- as.data.frame(tb.aln)
+rownames(final.df) <- c("Sequence 1", "Sequence 2")
+colnames(final.df) <- c(1:(length(seq1)-1))
+final.df
+
+
+# traceback itself
+for(i in length(seq1):2){
+  for(j in length(seq2):2){
+    x <- aln1.df[i,j]
+    y <- aln1.df[i+1, j]
+    z <- aln1.df[i, j+1]
+    if(x > y){
+      
+    }
+  }
+}
+
+
+############################## OLD CODE  - delete when final 
+
+# SEQUENCES TO ALIGN 
+# seq1 <- c(NA, "A", "C", "T", "G", "A", "T", "T", "C", "A")
+# seq2 <- c(NA, "A", "C", "G", "C", "A", "T", "C", "A")
+
+  
+
+  # for(i in aln1.df){
+  #   if(seq1[i] == seq2[i]){
+  #    x <- match
+  #    } else {
+  #      if(seq1[i] == seq2[i-1]){
+#        x <- 1
+#      } else { 
+#        if(seq1[i-1] == seq2[i]){ 
+#          x <- 1}
+#        }
+#    }
+#  if(x == 0){
+#    aln1.df[i+1,i+1] <- -2
+#  }
+#  x <- 0
+#}
+
+# for(i in aln1.df){
+#   print("hello")
+# }
+
+
