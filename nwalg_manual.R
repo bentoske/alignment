e6535b8e3028d723e7e61083aad1c6688155137e
Bentoske <ajmill38@asu.edu>
2020-08-19 15:34:43 -0700

    Added final traceback matrix, converted into a function needleman() and backtrace()

--- nwalg_manual.R
+++ nwalg_manual.R
@@ -1,69 +1,67 @@
-
-
-
-# string to vector
-seq1.st <- "ACCGGTAT"
-seq2.st <- "ACCTATC"
-
-seq1 <- c(NA, strsplit(seq1.st, split = "", useBytes = TRUE)[[1]])
-seq1
-seq2 <- c(NA, strsplit(seq2.st, split = "", useBytes = TRUE)[[1]])
-seq2
-
-# ---------------------------- build data frame
-aln1 <- matrix(, nrow = length(seq1), ncol = length(seq2))
-rownames(aln1) <- seq1
-colnames(aln1) <- seq2
-aln1.df <- as.data.frame(aln1)
-aln1.df
-
-# ---------------------------- copy data frame
-traceback.df <- as.data.frame(aln1.df)
-traceback.df
-
-# ---------------------------- assign variables
-gap <- -1
-match <- 2
-mismatch <- -2
-b <- 0
-r <- 0
-d <- 0 
-
-
-# ---------------------------- int rows
-for(i in 1:length(seq2)){
-  aln1.df[1,i] <- gap * (i-1)
-  traceback.df[1,i] <- "R"
-}
-aln1.df
-traceback.df
-
-# ---------------------------- int columns
-for(i in 1:length(seq1)){
-  aln1.df[i,1] <- gap * (i-1)
-  traceback.df[i,1] <- "B"
-}
-aln1.df
-traceback.df
-
-# ---------------------------- rest of matrix
-for(i in 2:length(seq1)){
-  for(j in 2:length(seq2)){
-    b <- aln1.df[i-1, j] + gap 
-    r <- aln1.df[i, j-1] + gap 
-    if(seq1[i] == seq2[j]){
-      d <- aln1.df[i-1, j-1] + match
-    } else {
-      d <- aln1.df[i-1, j-1] + mismatch
-    }
-  aln1.df[i,j] <- max(b,r,d)
-  m <- which.max(c(b,r,d))
-  traceback.df[i,j] <- ifelse(m == 1, "B", ifelse(m == 2, "R", "D") )
-  #traceback.df[i,j] <- ifelse(max(b,r,d) == b, "B", ifelse(max(b,r,d) == r, "R", "D") )
+needleman <- function(){
+  
+  # string to vector
+  seq1.st <- "ACCGGTAT"
+  seq2.st <- "ACCTATC"
+  
+  seq1 <- c(NA, strsplit(seq1.st, split = "", useBytes = TRUE)[[1]])
+  seq2 <- c(NA, strsplit(seq2.st, split = "", useBytes = TRUE)[[1]])
+  
+  # ---------------------------- build data frame
+  aln1 <- matrix(, nrow = length(seq1), ncol = length(seq2))
+  rownames(aln1) <- seq1
+  colnames(aln1) <- seq2
+  aln1.df <- as.data.frame(aln1)
+#  aln1.df
+  
+  # ---------------------------- copy data frame
+  traceback.df <- as.data.frame(aln1.df)
+#  traceback.df
+  
+  # ---------------------------- assign variables
+  gap <- -1
+  match <- 2
+  mismatch <- -2
+  b <- 0
+  r <- 0
+  d <- 0 
+  
+  
+  # ---------------------------- int rows
+  for(i in 1:length(seq2)){
+    aln1.df[1,i] <- gap * (i-1)
+    traceback.df[1,i] <- "R"
   }
+#  aln1.df
+#  traceback.df
+  
+  # ---------------------------- int columns
+  for(i in 1:length(seq1)){
+    aln1.df[i,1] <- gap * (i-1)
+    traceback.df[i,1] <- "B"
+  }
+#  aln1.df
+#  traceback.df
+  
+  # ---------------------------- rest of matrix
+  for(i in 2:length(seq1)){
+    for(j in 2:length(seq2)){
+      b <- aln1.df[i-1, j] + gap 
+      r <- aln1.df[i, j-1] + gap 
+      if(seq1[i] == seq2[j]){
+        d <- aln1.df[i-1, j-1] + match
+      } else {
+        d <- aln1.df[i-1, j-1] + mismatch
+      }
+    aln1.df[i,j] <- max(b,r,d)
+    m <- which.max(c(b,r,d))
+    traceback.df[i,j] <- ifelse(m == 1, "B", ifelse(m == 2, "R", "D") )
+    #traceback.df[i,j] <- ifelse(max(b,r,d) == b, "B", ifelse(max(b,r,d) == r, "R", "D") )
+    }
+  }
+#  aln1.df
+  return(traceback.df)
 }
-aln1.df
-traceback.df
 # ---------------------------- traceback
 
 ?which.max # explore this 
@@ -77,6 +75,53 @@ final.df
 
 # ----------------------------- traceback part 2 
 # use while loop? 
+
+backtrace = function(traceback.df){
+
+  # empty alignments
+  finalseq1 <- c()
+  finalseq2 <- c()
+
+  #reset i and j
+  i <- nrow(traceback.df)
+  j <- ncol(traceback.df)
+
+  while(i > 1 && j > 1){
+   if(traceback.df[i,j] == "D"){
+      finalseq1 <- c(rownames(traceback.df)[i], finalseq1)
+      finalseq2 <- c(colnames(traceback.df)[j], finalseq2)
+    j <- j - 1
+    i <- i - 1
+    } else if(traceback.df[i,j] == "B"){
+      finalseq1 <- c(rownames(traceback.df)[i], finalseq1)
+      finalseq2 <- c("-", finalseq2)
+      i <- i - 1
+    } else if(traceback.df[i,j] == "R"){
+      finalseq1 <- c("-", finalseq1)
+      finalseq2 <- c(colnames(traceback.df)[j], finalseq2)
+      j <- j - 1
+    } 
+  }
+  finalalignment <- rbind(finalseq1, finalseq2)
+  return(finalalignment)
+}
+
+alignment <- backtrace(traceback.df)
+alignment
+
+add <- function(a,b) {
+  return(a+b)
+}
+
+result <- add(2,3)
+
+traceback.df
+
+
+# next steps
+# figure out other traceback 
+# run script from terminal 
+# read sequences from FASTA 
 
 # old traceback 
 #for(i in length(seq1):2){
